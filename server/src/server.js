ea525cc58ae73e895e78095153f5fcb659a151e0
Nishit <nishit.pasupuleti@gmail.com>
2026-07-14 11:27:13 +0530

    Fix CORS

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+     origin: [
+        "http://localhost:5173",
+        "https://ai-personal-finance-platform.vercel.app"
+    ],
     credentials: true,
   })
 );
